3bda071e136d2ffe4f48814ccf34f2246aed371c
antoine <antoine.gayoso@gmail.com>
2021-07-13 11:23:31 +0200

    feat: added routers and locales

---
+++ src/constants/router.js
@@ -0,0 +1,13 @@
+export const PAGE_HOME = `/`;
+export const PAGE_REVIEWLIST = `/reviewlist/`;
+
+export const routes = [
+  {
+    name: "Home",
+    path: PAGE_HOME,
+  },
+  {
+    name: "Review details",
+    path: PAGE_REVIEWLIST,
+  },
+];
