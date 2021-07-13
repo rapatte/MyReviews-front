e4fa47f9493b9c13eec3c0f441d824dd6ec09a24
antoine <antoine.gayoso@gmail.com>
2021-07-13 14:50:14 +0200

    fix: eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     "react-hooks/exhaustive-deps": "warn",
     "react-hooks/rules-of-hooks": "error",
     "react/forbid-prop-types": 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/jsx-indent": 0,
     "react/jsx-indent-props": 0,
     "react/jsx-props-no-spreading": 0,
